f28993f3cfc624f318284b8adef150c86c9d6888
arstarkir <52779411+arstarkir@users.noreply.github.com>
2025-03-03 05:21:16 -0500

    Update renderer v2.js

--- USER/renderer v2.js
+++ USER/renderer v2.js
@@ -69,6 +69,14 @@ class Vector
     {
         return `Vec(${this.x},${this.y},${this.z})`
     }
+
+    perspectiveProj(camPlain, screenWidth, screenHeight, FOV, zFar)
+    {
+        let aspectR = screenHeight/screenWidth;
+        let scaleF = 1/(Math.tan(FOV/2));
+        let zNormalizer = (zFar/(zFar - camPlain.GetCenter().z));
+        return new Vector(new Point(0,0,0),new Point(aspectR*scaleF*this.x, scaleF*this.y,((this.z * zNormalizer -(zFar/(zFar - camPlain.GetCenter().z)*camPlain.GetCenter().z)))))
+    }
 }
 
 class Polygon 
@@ -78,6 +86,11 @@ class Polygon
         this.v1 = new Vector(p1, p2);
         this.v2 = new Vector(p2, p3);
         this.v3 = new Vector(p3, p1);
+    }
+
+    GetCenter()
+    {
+        return new Vector(new Point(0,0,0),new Point((this.v1.x + this.v2.x + this.v3.x)/3,(this.v1.y + this.v2.y + this.v3.y)/3,(this.v1.z + this.v2.z + this.v3.z)/3))
     }
 
     GetNormal()
@@ -107,13 +120,13 @@ let camPlain = new Polygon(new Point(3,3,0),new Point(2,4,0),new Point(3,5,0));
 const cubeObjJson = {
     "vertices": [
       { "x": 0, "y": 0, "z": 0 },
-      { "x": 1/6, "y": 0, "z": 0 },
-      { "x": 0, "y": 1/6, "z": 0 },
-      { "x": 1/6, "y": 1/6, "z": 0},
-      { "x": 0, "y": 0, "z": 1/6 },
-      { "x": 1/6, "y": 0, "z": 1/6 },
-      { "x": 0, "y": 1/6, "z": 1/6 },
-      { "x": 1/6, "y": 1/6, "z": 1/6}
+      { "x": 1, "y": 0, "z": 0 },
+      { "x": 0, "y": 1, "z": 0 },
+      { "x": 1, "y": 1, "z": 0},
+      { "x": 0, "y": 0, "z": 1 },
+      { "x": 1, "y": 0, "z": 1 },
+      { "x": 0, "y": 1, "z": 1 },
+      { "x": 1, "y": 1, "z": 1}
     ]
 };
 
@@ -142,9 +155,9 @@ let facesPyramid = [
 ];
 
 
-function loadMeshFromJSON(jsonData,faces) 
+function loadMeshFromJSON(jsonData,faces, mult) 
 {
-    let points = jsonData.vertices.map(v => new Point(v.x, v.y, v.z));
+    let points = jsonData.vertices.map(v => new Point(v.x * mult, v.y* mult, v.z* mult));
     let mesh = new Mesh();
 
     faces.forEach(face => {mesh.addPolygon(new Polygon(points[face[0]], points[face[1]], points[face[2]]));});
@@ -159,16 +172,16 @@ function drawMesh(mesh)
 {
     let bX = Graphics.createArrayBuffer(300, 300, 1, { msb: true });
     mesh.mesh.forEach( tri => { 
-        let l1 = tri.v1.projectOntoScreen(camPlain)
-        let l2 = tri.v2.projectOntoScreen(camPlain)
-        let l3 = tri.v3.projectOntoScreen(camPlain)
+        let l1 = tri.v1.perspectiveProj(camPlain,300,300,120,1)
+        let l2 = tri.v2.perspectiveProj(camPlain,300,300,120,1)
+        let l3 = tri.v3.perspectiveProj(camPlain,300,300,120,1)
 
         
-        bX.drawLine(l1.x,l1.y,l2.x,l2.y)
-        bX.drawLine(l1.x,l1.y,l3.x,l3.y)
-        bX.drawLine(l3.x,l3.y,l2.x,l2.y)
+        bX.drawLine(l1.x+150,l1.y+150,l2.x+150,l2.y+150)
+        bX.drawLine(l1.x+150,l1.y+150,l3.x+150,l3.y+150)
+        bX.drawLine(l3.x+150,l3.y+150,l2.x+150,l2.y+150)
     });
-
+    
     frameCounter++;
 
     bC.clear().drawImage(
@@ -193,7 +206,7 @@ function customTorchFunction()
         if (result === 1) 
         {
             onScreen = true;
-            mesh = loadMeshFromJSON(cubeObjJson,facesCube);
+            mesh = loadMeshFromJSON(cubeObjJson,facesCube, 100);
             drawMesh(mesh);
         } 
         else if (result === 2) 
@@ -201,14 +214,11 @@ function customTorchFunction()
             onScreen = true;
             bC.clear()
             mesh = loadMeshFromJSON(cubeObjJson,facesCube);
-            let l1 = new Vector(new Point(0,0,0),new Point(1,1,1));
-            let l2 = new Vector(new Point(0,0,0),new Point(1,1,1));
-            let l3 = new Vector(new Point(0,0,0),new Point(1,1,1));
             let text = "";
             mesh.mesh.forEach(tri => {
-                l1 = tri.v1.projectOntoScreen(camPlain, 300, 300);
-                l2 = tri.v2.projectOntoScreen(camPlain, 300, 300);
-                l3 = tri.v3.projectOntoScreen(camPlain, 300, 300);
+                let l1 = tri.v1.perspectiveProj(camPlain,300,300,120,10)
+                let l2 = tri.v2.perspectiveProj(camPlain,300,300,120,10)
+                let l3 = tri.v3.perspectiveProj(camPlain,300,300,120,10)
                 text += `${l1.toString()},${l2.toString()},${l3.toString()}
                 `;
             });
@@ -223,17 +233,21 @@ function customTorchFunction()
             let l1 = new Vector(new Point(0,0,0),new Point(1,1,1));
             text += `V ${l1.toString()},
             `
-            let normal = camPlain.GetNormal();
-            text += `N ${normal.toString()},
+            let aspectR = 300/300;
+            let scaleF = 1/(Math.tan(120/2));
+            let zNormalizer = (10/(10 - camPlain.GetCenter().z));
+            text += `AR ${300/300},
             `
-            text += `B1 ${l1.dot(normal)},
+            text += `F ${1/(Math.tan(120/2))},
             `            
-            text += `B2 ${(Math.pow(normal.dist(),2))},
+            text += `zN ${(10/(10 - camPlain.GetCenter().z))},
             `
-            let proj = l1.subtract(normal.multiply(l1.dot(normal)/(Math.pow(normal.norm(),2))))
+            let proj = new Vector(new Point(0,0,0), new Point(aspectR*scaleF*l1.x, scaleF*l1.y,((l1.z * zNormalizer -(10/(10 - camPlain.GetCenter().z)*camPlain.GetCenter().z)))))
             text += `${proj.toString()},
             `
+
             Pip.typeText(`(${text})`).then(() => {setTimeout(() => {}, 10);});
+
             //rotMod = !rotMod;
         }
         else if (result === 4) 
